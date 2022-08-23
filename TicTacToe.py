98c5634676e03a417a7101599bda8d264b4eddeb
ALkonrad <kaunelundberg@gmail.com>
2022-08-23 18:46:41 +0200

    Added the possibility to win

--- TicTacToe.py
+++ TicTacToe.py
@@ -12,12 +12,31 @@ def draw_board():
 
 def add_symbol(symbol):
     symbol_location = int(input('>>> ')) - 1
-    if game_board[symbol_location] == 'x' or game_board[symbol_location] == 'o':
-        print('no')
+    if symbol_location > 9:
+        print('The number you have entered is too large.')
+        add_symbol(symbol)
+    elif game_board[symbol_location] == 'x' or game_board[symbol_location] == 'o':
+        print('This square is taken.')
         add_symbol(symbol)
     else:
         game_board[symbol_location] = symbol
         draw_board()
+        check_if_won()
+
+
+def check_if_won():
+    winning_combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6],
+    ]
+    for current_combination in winning_combinations:
+        if game_board[current_combination[0]] == game_board[current_combination[1]] == game_board[current_combination[2]] == 'x':
+            print('x wins')
+            exit()
+        elif game_board[current_combination[0]] == game_board[current_combination[1]] == game_board[current_combination[2]] == 'o':
+            print('o wins')
+            exit()
 
 
 current_symbol = 'x'
